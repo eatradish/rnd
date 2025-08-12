b54fb4c2adc7eac65e6ed607e4188c796a09abd6
eatradish <sakiiily@aosc.io>
2025-08-12 13:46:59 +0800

    feat: improve example

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct App {
     /// Get result count
     #[arg(short = 'n', long, default_value_t = 1)]
     count: usize,
-    /// Input range (like 1..=10)
+    /// Input range (like 114514..=1919810)
     #[arg(allow_hyphen_values = true)]
     range: Option<String>,
 }
